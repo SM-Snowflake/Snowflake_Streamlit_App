cebb7415024be6598283a2efc38f5d6e1e19796f
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-08 19:40:04 +0100

    Update streamlit_app.py
    Added Emojis
    Added another breakfast item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal\n'
-                'Kale, Spinach & Rocket Smoothie\n'
-                'Hard-boiled Free-Range Egg')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal\n'
+              ':green_salad: Kale, Spinach & Rocket Smoothie\n'
+              ':chicken: Hard-boiled Free-Range Egg\n'
+              ':avocado: :toast: Avocado Toast')
