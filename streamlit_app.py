dbb92b66725ff0d70216146742f1c959b0ec095e
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-08 20:13:00 +0100

    Update streamlit_app.py
    Corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #Adding a pick list, so that customers can pick the fruits they want to include
-fruit_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
