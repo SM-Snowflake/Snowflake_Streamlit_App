90e69f5e1851eb9085b4e106186589dc38417643
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-08 20:04:24 +0100

    Update streamlit_app.py
    Added fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
+
+#Adding a pick list, so that customers can pick the fruits they want to include
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+
+#display the table on the page
+streamlit.dataframe(my_fruit_list)
+
