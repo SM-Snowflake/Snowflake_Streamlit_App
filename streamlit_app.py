b395b555235f186cd65826d9f7b7427c06c1b122
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-09 01:45:41 +0100

    Update streamlit_app.py
    Cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas as pd
+#import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
