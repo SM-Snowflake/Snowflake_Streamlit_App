3153ccd9474d9e62a330b8d567a4db1c525b0ab3
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-09 01:39:38 +0100

    Update streamlit_app.py
    Correcting Error in line 49

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ streamlit.header('The fruit load list contains:')
 streamlit.dataframe(my_data_rows)
 
 fruit_add = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', fruit_add)
