0f663ea069e1f1ff4f04454250aad7954a201157
SM-Snowflake <115369762+SM-Snowflake@users.noreply.github.com>
2022-10-09 01:50:40 +0100

    Update streamlit_app.py
    Restructured Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #take the json version of the response and normalize it
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    #output it on the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+  except URLError as e:
+    streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-#output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 #stopping streamlit while the code is improved
 streamlit.stop()
